0441224689a9aefe7f63abd51ddff17bb948b6a6
aamir0143 <mdaamirreza00@gmail.com>
2022-01-29 17:44:41 +0530

    [Aamir3] Add : To Calculate The Length

--- LineComparisonProblem/Program.cs
+++ LineComparisonProblem/Program.cs
@@ -1,3 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("==========Welcome To Line Comparison Computation Program==========");
-Console.ReadLine();
+using System;
+namespace LineComparisonProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==========Welcome To Line Comparison Computation Program==========");
+            LineLength length = new LineLength();
+            length.CalculateLength(); //Call The Methods
+            Console.ReadLine();
+        }
+    }
+}
